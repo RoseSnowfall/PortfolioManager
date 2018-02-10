cc469f9974beb8addc9499f6b98ced4b641b1a44
RoseSnowfall <31056339+RoseSnowfall@users.noreply.github.com>
2018-02-10 14:07:00 -0700

    ni hao

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 console.log('Let\'s begin.');
-setTimeout(function(){console.log('We did it!haha yes')}, 3000);
+setTimeout(function(){console.log('We did it!haha yes no i dont know just try it')}, 3000);
